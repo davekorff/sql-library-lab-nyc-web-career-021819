3af32fd682427a0c66f427532052ec788e676e10
David Korff <dkorff12@gmail.com>
2019-02-28 21:17:47 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title) VALUES ("HP");
+INSERT INTO series (title) VALUES ("HP");
+
+INSERT INTO books (title) VALUES ("HP1");
+INSERT INTO books (title) VALUES ("HP1");
+INSERT INTO books (title) VALUES ("HP1");
+INSERT INTO books (title) VALUES ("HP1");
+INSERT INTO books (title) VALUES ("HP1");
+INSERT INTO books (title) VALUES ("HP1");
+
+INSERT INTO characters (name) VALUES ("Harry");
+INSERT INTO characters (name) VALUES ("Harry");
+INSERT INTO characters (name) VALUES ("Harry");
+INSERT INTO characters (name) VALUES ("Harry");
+INSERT INTO characters (name) VALUES ("Harry");
+INSERT INTO characters (name) VALUES ("Harry");
+INSERT INTO characters (name) VALUES ("Harry");
+INSERT INTO characters (name) VALUES ("Harry");
+
+INSERT INTO subgenres (name) VALUES ("YA");
+INSERT INTO subgenres (name) VALUES ("YA");
+
+INSERT INTO authors (name) VALUES ("JK");
+INSERT INTO authors (name) VALUES ("JK");
+
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
+INSERT INTO character_books (character_id) VALUES (1);
